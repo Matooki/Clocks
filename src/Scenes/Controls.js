448d66478021b333f53f1ba641db1d294581cb43
Dallas Truong <damtruon@ucsc.edu>
2020-06-08 12:12:40 -0700

    Fixed Bugs

--- src/Scenes/Controls.js
+++ src/Scenes/Controls.js
@@ -1,6 +1,6 @@
 class Controls extends Phaser.Scene {
     constructor() {
-        super("constrolsScene");
+        super("controlsScene");
     }
 
     create()
@@ -8,7 +8,7 @@ class Controls extends Phaser.Scene {
         // display mennu
         let controlConfig = {
             fontFamily: 'fantasy',
-            fontSize: '16px',
+            fontSize: '11px',
             backgroundColor: '#7aa1a0',
             color: '#7b348c',
             align: 'center',
@@ -27,10 +27,11 @@ class Controls extends Phaser.Scene {
         this.add.text(centerX, centerY- textSpacer, 'Press S to Shrink and G to Grow', controlConfig).setOrigin(0.5);
         this.add.text(centerX, centerY + 0 * textSpacer, 'Some door open when you walk into them with keys', controlConfig).setOrigin(0.5);
         this.add.text(centerX, centerY + 1 * textSpacer, 'Some doors open when you walk into them after activating something', controlConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY + 1 * textSpacer, 'Press I to return to Menu', controlConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + 2 * textSpacer, 'Press I to return to Menu', controlConfig).setOrigin(0.5);
 
-        this.swap = this.input.keyboard.addKey('N');
         cursors = this.input.keyboard.createCursorKeys();
+        this.swap = this.input.keyboard.addKey('N');
+        this.controls = this.input.keyboard.addKey('I');
         //keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Down);
     }
 
